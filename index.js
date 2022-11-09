38f7c3fe762076a07ce05c41c02263b0efdf6585
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-11-10 00:18:12 +0600

    Added services to server and added a services route to get data

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zuoxzfe.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zuoxzfe.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+  try{
+    const serviceCollection = client.db('skConsultancy').collection('services')
 
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query)
+      const services = await cursor.toArray()
+
+      res.send(services)
+    })
+  } finally {
+
+  }
+}
+
+run().catch(err => console.log(err))
 
 app.get("/", (req, res) => {
   res.send("Server is Running")
